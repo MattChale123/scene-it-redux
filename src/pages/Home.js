c4b4a44b4b304579cc07c43066d52135ec18a5d5
Matthew Chaleski <chaleski76@gmail.com>
2021-04-05 16:36:08 -0400

    React set up!!

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home(props) {
+    return (
+        <div>
+            <h1>
+            Have you scene it?
+          </h1>
+        </div>
+    )
+}
